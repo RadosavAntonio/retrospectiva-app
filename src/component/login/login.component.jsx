1b836211508cafa33d201ba39307557e2a5582c9
Antonio Radosav <antonioradosav@Antonios-MacBook-Pro.local>
2021-03-02 10:35:47 +0000

    am adaugat cate ceva pt git log in

--- src/component/login/login.component.jsx
+++ src/component/login/login.component.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import * as LS from './login.style'
+import { Button, TextField } from "@material-ui/core";
+import React, { useState } from "react";
+import * as LS from "./login.style";
+
+const user = window.localStorage.getItem('retrospective-app') 
+  ? JSON.parse(window.localStorage.getItem('retrospective-app'))
+  : ''
+console.log(';########################')
+console.log(user)
 
 export const Login = () => {
-  return(
+
+  
+
+
+  return (
     <div>
       <LS.Layout>This is the Login page.</LS.Layout>
+      <div>
+        <TextField />
+      </div>
+      <div>
+        <TextField type="password" />
+      </div>
+      {user && <div>
+        <Button
+          variant="outlined"
+          color="secondary"
+          type="submit"
+          // onClick={iesiAcasa}
+        >
+          Logout
+        </Button>
+      </div>}
+      {!user && <div>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          // onClick={bagaTare}
+        >
+          Login
+        </Button>
+      </div>}
     </div>
-  )
-}
+  );
+};
